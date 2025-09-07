d139ba8a22d6b9d5a80922ef5818d7fc0eadabf0
Sahil Chugh <chughs24@gmail.com>
2025-09-07 23:34:26 +0100

    feat: handle single number input parsing

--- lib/src/string_calculator_assessment.dart
+++ lib/src/string_calculator_assessment.dart
@@ -1,5 +1,9 @@
 int add(String input) {
   // if empty string, return 0
   if (input.isEmpty) return 0;
+  // if only one number, return it
+  if (!input.contains(',')) {
+    return int.parse(input);
+  }
   return 0;
 }
